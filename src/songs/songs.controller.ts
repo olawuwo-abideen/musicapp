37b0c5cdece278d9f329dee35fdb9705b22a0d87
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-05-20 17:31:49 +0100

    dependency injection

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,20 +1,59 @@
-import { Controller, Get, Put, Delete, Post } from '@nestjs/common';
-import { SongsService } from './songs.service';
+import {
+    Controller,
+    Get,
+    Put,
+    Delete,
+    Post,
+    HttpException,
+    HttpStatus,
+    Param,
+    ParseIntPipe,
+    Body,
+    Inject
+  } from '@nestjs/common';
+  import { SongsService } from './songs.service';
+  import { CreateSongDTO } from './dto/create-song-dto';
+  import { Connection } from 'src/common/constant/connection';
 
+  
 @Controller('songs')
 export class SongsController {
-  constructor(private songsService: SongsService) {}
+  constructor(
+    private songsService: SongsService,
+    @Inject('CONNECTION')
+    private connection: Connection,
+  ) {
+    console.log(
+      `THIS IS CONNECTION STRING ${this.connection.CONNECTION_STRING}`,
+    );
+  }
   @Post()
-  create() {
-    return this.songsService.create('create new songs');
+  create(@Body() createSongDTO: CreateSongDTO) {
+    return this.songsService.create(createSongDTO);
   }
   @Get()
   findAll() {
-    return this.songsService.findAll();
+    try {
+      return this.songsService.findAll();
+    } catch (e) {
+      throw new HttpException(
+        'server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
+    }
   }
   @Get(':id')
-  findOne() {
-    return 'fetch song on the based on id';
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    return `fetch song on the based on id ${typeof id}`;
   }
 
   @Put(':id')
--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
+import { connection } from 'src/common/constant/connection';
 
+const mockSongsService = {
+  findAll() {
+    return [{ id: 1, title: 'Lasting ', artists: ['boy'] }];
+  },
+};
 
 @Module({
   controllers: [SongsController],
-  providers: [SongsService]
+  providers: [
+    SongsService,
+    // {
+    //   provide: SongsService,
+    //   useClass: SongsService,
+    // },
+    // {
+    //   provide: SongsService,
+    //   useValue: mockSongsService,
+    // },
+    {
+      provide: 'CONNECTION',
+      useValue: connection,
+    },
+  ],
 })
 export class SongsModule {}
