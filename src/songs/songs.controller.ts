323a403f8ac4faf29d0eae81ff7973587339d3f4
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2024-05-20 09:53:53 +0100

    update controller

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,5 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Put, Delete, Post } from '@nestjs/common';
+import { SongsService } from './songs.service';
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+  constructor(private songsService: SongsService) {}
+  @Post()
+  create() {
+    return this.songsService.create('create new songs');
+  }
+  @Get()
+  findAll() {
+    return this.songsService.findAll();
+  }
+  @Get(':id')
+  findOne() {
+    return 'fetch song on the based on id';
+  }
 
+  @Put(':id')
+  update() {
+    return 'update song on the based on id';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'delete song on the based on id';
+  }
+}
--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
 
+
 @Module({
   controllers: [SongsController],
   providers: [SongsService]
